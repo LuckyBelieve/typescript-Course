295d44451c33b0c9596b095a60e74a67b0d2e291
LuckyBelieve <luckybelieve@gmail.com>
2023-07-17 10:44:48 +0200

    arrays in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-let number:number = 2000;
-console.log(number);
+//arrays in typescript
+//example
+let number:number[] = [1,2,3,4];
+
+let people:string[] = [];
+people[1] = 'John';
+people[2] = 'Mary';
+people.forEach(person => {
+    console.log(person);
+});
+
+//when you don't annotate the type while declaring variables the default type is any
+
